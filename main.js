cace26b70a5f1d8440c34def8244b60f782d5851
sahhas16 <sahhas2008@gmail.com>
2020-10-02 14:48:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,4 +11,20 @@ recognition.onresult=function(event){
     var text=event.results[0][0].transcript;
     console.log(text);
     document.getElementById("textbox").innerHTML=text;
+    speech();
+    Webcam.attach(camera);
+    
 }
+function speech(){
+    var speech_object=window.speechSynthesis;
+    var words=document.getElementById("textbox").value;
+    var uttered_word=new SpeechSynthesisUtterance(words);
+    speech_object.speak(uttered_word);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+    width:320,
+    height:240,
+    image_format:'png',
+    png_quality:90
+});
